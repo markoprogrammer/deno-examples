ba9a82d21c8124ae39310d770da17a79219d1476
Marko Manojlovic <markomanojlovic@Markos-MacBook-Pro.local>
2020-09-02 23:34:18 +0200

    update app server

--- deno-react/app-server.ts
+++ deno-react/app-server.ts
@@ -5,6 +5,6 @@ import App from "./app.tsx";
 // Update `appModulePath` from the example React component to your own.
 baseServer({
   appModulePath:
-    App,
+    'https://raw.githubusercontent.com/markoprogrammer/deno-examples/master/deno-react/app.tsx',
   port: 3001,
 });
